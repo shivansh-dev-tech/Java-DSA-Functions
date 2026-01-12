f2a6b4927df50b47547973e2761aaa54b15374e5
Shivansh Sharma <shiv2006sharma@gmail.com>
2026-01-13 00:20:42 +0530

    Add product of two numbers using functions
    Added a method to multiply two integers and demonstrated its usage in the main method.

--- Functions/ProductOfTwoNumbers.java
+++ Functions/ProductOfTwoNumbers.java
@@ -1,1 +1,17 @@
-
+//program: Product of two numbers using Functions
+//concept used: Functions and Return type
+public class ProductOfTwoNumbers{
+    public static int multiply(int a , int b){
+        int product =  a * b;
+        return product;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 5;
+        int b = 3;
+        int prod = multiply(a,b);
+        System.out.println("a * b:"+ prod);
+        prod = multiply(100, 200);
+        System.out.println("a * b:"+ prod);
+    }
+}
